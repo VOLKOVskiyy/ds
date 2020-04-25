146ba416761208049354bfdaabe54c5618d76c8b
VOLKOVskiyy <64324232+VOLKOVskiyy@users.noreply.github.com>
2020-04-26 00:00:27 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+TOKEN = 'NzAzNjI4OTE5NDMzMDY4NTk0.XqRXkg.RGelZAqOmqpDruMJ2ml7bnmJHBo'
+
+POST_ID = 703640957714956408 
+
+ROLES = {
+    '🔥': 702874475376410725, #volkovskiy
+    '🎲': 702875182863482891, #samper
+    '⚡': 702875293441982516, #free
+    '🎮': 702875462770098196, #cser
+    '🌀': 702875463864942592, #bitch
+    '💥': 702875992909283482, #gangshitbang
+    '💯': 702877506964815985, #100iq
+
+}
+
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 1
